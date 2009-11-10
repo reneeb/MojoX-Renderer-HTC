d7ed8d9fd1a1e05feb1f132b3446dc238b32ddfc
unknown <Entwicklung@.(none)>
2009-11-10 23:35:37 +0100

    initial commit

---
+++ lib/MojoX/Renderer/HTC.pm
@@ -0,0 +1,110 @@
+package MojoX::Renderer::HTC;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+MojoX::Renderer::HTC - The great new MojoX::Renderer::HTC!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use MojoX::Renderer::HTC;
+
+    my $foo = MojoX::Renderer::HTC->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Renee Baecker, C<< <module at renee-baecker.de> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-mojox::renderer::htc at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MojoX::Renderer::HTC>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc MojoX::Renderer::HTC
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MojoX::Renderer::HTC>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/MojoX::Renderer::HTC>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/MojoX::Renderer::HTC>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/MojoX::Renderer::HTC/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Renee Baecker.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of MojoX::Renderer::HTC
