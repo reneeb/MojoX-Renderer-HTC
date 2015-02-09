877f8ef0964f84d4e52712efdffd3a701b757d27
reneeb <github@renee-baecker.de>
2015-02-09 11:04:13 +0100

    remove pod: the content is generated by Pod::Weaver

--- lib/MojoX/Renderer/HTC.pm
+++ lib/MojoX/Renderer/HTC.pm
@@ -5,18 +5,9 @@ use strict;
 
 use HTML::Template::Compiled;
 
-=head1 NAME
+# ABSTRACT: HTML::Template::Compiled renderer for Mojo
 
-MojoX::Renderer::HTC - HTML::Template::Compiled renderer for Mojo
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
@@ -59,64 +50,5 @@ sub build {
         $$output = $template->output;
     }
 }
-
-=head1 AUTHOR
-
-Renee Baecker, C<< <module at renee-baecker.de> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-mojox::renderer::htc at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MojoX::Renderer::HTC>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc MojoX::Renderer::HTC
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MojoX::Renderer::HTC>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/MojoX::Renderer::HTC>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/MojoX::Renderer::HTC>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/MojoX::Renderer::HTC/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Renee Baecker.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
-
-=cut
 
 1; # End of MojoX::Renderer::HTC
